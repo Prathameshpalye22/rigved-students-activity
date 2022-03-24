2450ff93c9e5e044d57a7c0de02d9c867bf2e7f3
Prathameshpalye22 <prathameshpalye1212@gmail.com>
2022-03-24 17:24:12 +0530

    Day23 Activity Done

---
+++ Node JS example/day23/ex3http.js
@@ -0,0 +1,12 @@
+let http = require('http');
+const { resolve } = require('path');
+let PORT_NO = 3001;
+http
+.createServer((request, response)=> {
+    let username ='Prathamesh';
+    response.writeHead(200, {'content-type': 'text/html'});
+    response.write(`<h2>Hello App</h2>`);
+    response.write(`<h3>Username: ${username}</h3>`);
+    response.end();
+})
+.listen(PORT_NO, () => console.log(`Server is running in ${PORT_NO}`));
