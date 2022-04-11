b10b673660625d80277798486275de4c0d611fe2
Prathameshpalye22 <prathameshpalye1212@gmail.com>
2022-04-11 17:11:59 +0530

    day35 activity done

---
+++ Angular example/day35/eighth-app/src/app/activity/activity.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-activity',
+  templateUrl: './activity.component.html',
+  styleUrls: ['./activity.component.css']
+})
+export class ActivityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+      firstname= new FormControl('');
+      lastname= new FormControl('');
+   
+  
+}
